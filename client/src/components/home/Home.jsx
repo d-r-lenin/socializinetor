2629ca25724ac2cfc7ddda86b0feff3c264de8a0
d-r-lenin <drlenin2002@gmail.com>
2023-04-21 21:21:41 +0530

    redux init

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../slices/posts';
 
 import EndlessDeathScroll from '../endlessDeathScroll/EndlessDeathScroll';
 
 function Home() {
+  const counter = useSelector(state => state.post.value);
+  console.log(counter);
+  const dispatch = useDispatch();
   return (
     <div>
         <div>
+        <p>{counter}</p>
+          <button onClick={() => dispatch(increment())}>+</button>
             <EndlessDeathScroll/>
         </div>
     </div>
   )
 }
 
-export default Home 
+export default Home;
